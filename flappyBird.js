23475fdc9c45fb120d8ac9a0a55bb90110f0b6d9
Shraddha Bhuju <shraddha.bhuju@gmail.com>
2021-07-10 20:34:42 +0545

    sprite rotation

--- flappyBird.js
+++ flappyBird.js
@@ -4,6 +4,7 @@ const ctx = cvs.getContext("2d");
 //Variables and constants
 
 let frames = 0;
+const DEGREE = Math.PI/180;
 
 // Sprites
 const sprite = new Image();
@@ -54,16 +55,25 @@ const fg= {
     h : 112,
     x: 0,
     y : cvs.height- 112, 
+    dx:2,
     draw : function(){
         ctx.drawImage(sprite, this.sX,this.sY,this.w,this.h,this.x,this.y,this.w,this.h);
         ctx.drawImage(sprite, this.sX,this.sY,this.w,this.h,this.x+this.w,this.y,this.w,this.h);
        
+    },
+    update: function()
+    {
+        if(state.current == state.game)
+        {
+           this.x = (this.x-this.dx) %(this.w/2);
+        }
     }
 
 }
  
 //Bird
 const bird= {
+
     animation :[
         { sX : 276, sY : 112 },
         { sX : 276, sY : 139},
@@ -75,26 +85,78 @@ const bird= {
         w: 34,
         h: 26,
         frame :0,
+        speed:0,
+        gravity : 0.25,
+        jump:4.6,
+        rotation:0,
+
 
   
     draw : function(){
         let bird= this.animation[this.frame];
-        ctx.drawImage(sprite, bird.sX,bird.sY,this.w,this.h,this.x-this.w/2,this.y-this.w/2,this.w,this.h);
-        
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(this.rotation)
+        ctx.drawImage(sprite, bird.sX,bird.sY,this.w,this.h,-this.w/2,-this.w/2,this.w,this.h);
+        ctx.restore();
        
     },
     flap :function(){
-
+        this.speed= -this.jump;
     },
     update: function()
     {
         this.period =state.current == state.getReady? 10:5;
         this.frame += frames%this.period==0?1:0;
         this.frame = this.frame%this.animation.length;
+        if(state.current == state.getReady)
+        {
+            this.y =150;
+            this.rotation = 0*DEGREE;
+        }
+        else{
+            this.speed += this.gravity;
+            this.y += this.speed;
+            if(this.y + this.h/2 >= cvs.height -fg.h )
+            { 
+                this.y= cvs.height -fg.h-this.h/2;
+                if(state.current == state.game)
+                {
+                    state.current = state.over; 
+                }
+               
+            }
+            if(this.speed >= this.jump)
+            {
+                this.rotation = 90* DEGREE;
+                this.frame=1;
+               
+            }
+            else{
+              this.rotation=-25*DEGREE;       
+
+            }
+        }
     }
 
 }
+//pipes
+const pipes ={
+    bottom:{
 
+    },
+    top:{
+
+    },
+    w:,
+    h:,
+    gap:,
+    dx:,
+    
+    draw : function(){
+
+    }
+}
 //Get ready
 
 const getReady= {
@@ -151,6 +213,7 @@ function draw()
 function update()
 {
     bird.update();
+    fg.update();
 }
 
 //Loop function
